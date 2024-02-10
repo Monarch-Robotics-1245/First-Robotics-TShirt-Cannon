3c7abab3312c05df4d70886b4f592c3fc76f52c8
Matthew Spratlin <mkspratlin01@bvsd.org>
2024-02-09 20:40:08 -0700

    Updated for new solenoid

--- src/main/java/frc/robot/subsystems/Cannon.java
+++ src/main/java/frc/robot/subsystems/Cannon.java
@@ -4,19 +4,21 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Solenoid;
 import frc.robot.Constants;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 
 
 public class Cannon extends SubsystemBase {
   /** Creates a new DriveTrain. */
 
-  private final DoubleSolenoid SEL_1 = new DoubleSolenoid(Constants.PCM, PneumaticsModuleType.CTREPCM, 0, 1);
-  private final DoubleSolenoid SEL_2 = new DoubleSolenoid(Constants.PCM, PneumaticsModuleType.CTREPCM, 2, 3);
-  private final DoubleSolenoid SEL_3 = new DoubleSolenoid(Constants.PCM, PneumaticsModuleType.CTREPCM, 4, 5);
+  private final Solenoid CANNON_ONE = new Solenoid(Constants.PCM, PneumaticsModuleType.CTREPCM, 1);
+  private final Solenoid CANNON_TWO = new Solenoid(Constants.PCM, PneumaticsModuleType.CTREPCM, 2);
+  private final Solenoid CANNON_THREE = new Solenoid(Constants.PCM, PneumaticsModuleType.CTREPCM, 3);
+  private final Solenoid CANNON_FOUR = new Solenoid(Constants.PCM, PneumaticsModuleType.CTREPCM, 4);
+  private final Solenoid CANNON_FIVE = new Solenoid(Constants.PCM, PneumaticsModuleType.CTREPCM, 5);
+  private final Solenoid CANNON_SIX = new Solenoid(Constants.PCM, PneumaticsModuleType.CTREPCM, 6);
 
   public Cannon() {
 
@@ -28,40 +30,61 @@ public class Cannon extends SubsystemBase {
 
   public void activateMaster(boolean active, int cannon) {
     if (active) {
-      if (cannon == Constants.S_ONE_F) {
-        SEL_1.set(kForward);
-        SEL_2.set(kOff);
-        SEL_3.set(kOff);
+      if (cannon == Constants.CANNON_ID_ONE) {
+        CANNON_ONE.set(true);
+        CANNON_TWO.set(false);
+        CANNON_THREE.set(false);
+        CANNON_FOUR.set(false);
+        CANNON_FIVE.set(false);
+        CANNON_SIX.set(false);
       }
-      if (cannon == Constants.S_ONE_R) {
-        SEL_1.set(kReverse);
-        SEL_2.set(kOff);
-        SEL_3.set(kOff);
+      if (cannon == Constants.CANNON_ID_TWO) {
+        CANNON_ONE.set(false);
+        CANNON_TWO.set(true);
+        CANNON_THREE.set(false);
+        CANNON_FOUR.set(false);
+        CANNON_FIVE.set(false);
+        CANNON_SIX.set(false);
       }
-      if (cannon == Constants.S_TWO_F) {
-        SEL_1.set(kOff);
-        SEL_2.set(kForward);
-        SEL_3.set(kOff);
+      if (cannon == Constants.CANNON_ID_THREE) {
+        CANNON_ONE.set(false);
+        CANNON_TWO.set(false);
+        CANNON_THREE.set(true);
+        CANNON_FOUR.set(false);
+        CANNON_FIVE.set(false);
+        CANNON_SIX.set(false);
       }
-      if (cannon == Constants.S_TWO_R) {
-        SEL_1.set(kOff);
-        SEL_2.set(kReverse);
-        SEL_3.set(kOff);
+      if (cannon == Constants.CANNON_ID_FOUR) {
+        CANNON_ONE.set(false);
+        CANNON_TWO.set(false);
+        CANNON_THREE.set(false);
+        CANNON_FOUR.set(true);
+        CANNON_FIVE.set(false);
+        CANNON_SIX.set(false);
       }
-      if (cannon == Constants.S_THREE_F) {
-        SEL_1.set(kOff);
-        SEL_2.set(kOff);
-        SEL_3.set(kForward);
+      if (cannon == Constants.CANNON_ID_FIVE) {
+        CANNON_ONE.set(false);
+        CANNON_TWO.set(false);
+        CANNON_THREE.set(false);
+        CANNON_FOUR.set(false);
+        CANNON_FIVE.set(true);
+        CANNON_SIX.set(false);
       }
-      if (cannon == Constants.S_THREE_R) {
-        SEL_1.set(kOff);
-        SEL_2.set(kOff);
-        SEL_3.set(kReverse);
+      if (cannon == Constants.CANNON_ID_SIX) {
+        CANNON_ONE.set(false);
+        CANNON_TWO.set(false);
+        CANNON_THREE.set(false);
+        CANNON_FOUR.set(false);
+        CANNON_FIVE.set(false);
+        CANNON_SIX.set(true);
       }
     } else {
-      SEL_1.set(kOff);
-      SEL_2.set(kOff);
-      SEL_3.set(kOff);
+      CANNON_ONE.set(false);
+      CANNON_TWO.set(false);
+      CANNON_THREE.set(false);
+      CANNON_FOUR.set(false);
+      CANNON_FIVE.set(false);
+      CANNON_SIX.set(false);
     }
   }
 }
